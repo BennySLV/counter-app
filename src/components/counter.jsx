276e12496e9cf0cdf11f91174d8f786e3436d940
Ben Silveston <bsilveston37@gmail.com>
2020-08-17 15:37:35 +0100

    Fixed bug in getBadgeClasses method.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,20 +10,29 @@ class Counter extends Component {
 		fontWeight: "bold",
 	};
 
+	handleIncrement() {
+		console.log("Increment clicked");
+	}
+
 	render() {
 		return (
 			<React.Fragment>
 				<span style={this.styles} className={this.getBadgeClasses()}>
 					{this.formatCount()}
 				</span>
-				<button className="btn btn-secondary btn-sm">Increment</button>
+				<button
+					onClick={this.handleIncrement}
+					className="btn btn-secondary btn-sm"
+				>
+					Increment
+				</button>
 			</React.Fragment>
 		);
 	}
 
 	getBadgeClasses() {
 		let classes = "badge m-2 badge-";
-		classes += this.state === 0 ? "warning" : "primary";
+		classes += this.state.count === 0 ? "warning" : "primary";
 		return classes;
 	}
 
