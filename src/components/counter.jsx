935a592766a1710c8c241b27c3bcdb461f9624ce
Ben Silveston <bsilveston37@gmail.com>
2020-08-17 15:33:09 +0100

    Removed sample tags.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 class Counter extends Component {
 	state = {
 		count: 0,
-		tags: ["tag1", "tag2", "tag3"],
 	};
 
 	styles = {
@@ -18,11 +17,6 @@ class Counter extends Component {
 					{this.formatCount()}
 				</span>
 				<button className="btn btn-secondary btn-sm">Increment</button>
-				<ul>
-					{this.state.tags.map((tag) => (
-						<li key={tag}>{tag}</li>
-					))}
-				</ul>
 			</React.Fragment>
 		);
 	}
