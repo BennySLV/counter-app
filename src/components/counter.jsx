7ef1466cf5e97f5f07ca8ff37dcb470673289698
Ben Silveston <bsilveston37@gmail.com>
2020-08-18 11:36:30 +0100

    Updated state on counter.jsx and removed unnecessary property in counters.jsx.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
 	state = {
-		count: 0,
+		value: this.props.value,
 	};
 
 	// constructor() {
@@ -17,10 +17,12 @@ class Counter extends Component {
 
 	handleIncrement = (product) => {
 		console.log(product);
-		this.setState({ count: this.state.count + 1 });
+		this.setState({ value: this.state.value + 1 });
 	};
 
 	render() {
+		console.log("props", this.props);
+
 		return (
 			<React.Fragment>
 				<span style={this.styles} className={this.getBadgeClasses()}>
@@ -38,12 +40,12 @@ class Counter extends Component {
 
 	getBadgeClasses() {
 		let classes = "badge m-2 badge-";
-		classes += this.state.count === 0 ? "warning" : "primary";
+		classes += this.state.value === 0 ? "warning" : "primary";
 		return classes;
 	}
 
 	formatCount() {
-		const { count } = this.state;
+		const { value: count } = this.state;
 		return count === 0 ? "Zero" : count;
 	}
 }
