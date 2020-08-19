632699888fda19dcf4cc783543dfd33645229af8
Ben Silveston <bsilveston37@gmail.com>
2020-08-19 14:06:28 +0100

    Removed the component's local state.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-	state = {
-		value: this.props.counter.value,
-	};
-
 	// Fallback constructor - in case handleIncrement method fails in the future
 	// constructor() {
 	// 	super();
@@ -16,10 +12,6 @@ class Counter extends Component {
 		fontWeight: "bold",
 	};
 
-	handleIncrement = () => {
-		this.setState({ value: this.state.value + 1 });
-	};
-
 	render() {
 		return (
 			<div>
@@ -27,7 +19,7 @@ class Counter extends Component {
 					{this.formatCount()}
 				</span>
 				<button
-					onClick={this.handleIncrement}
+					onClick={() => this.props.onIncrement(this.props.counter)}
 					className="btn btn-secondary btn-sm"
 				>
 					Increment
@@ -44,12 +36,12 @@ class Counter extends Component {
 
 	getBadgeClasses() {
 		let classes = "badge m-2 badge-";
-		classes += this.state.value === 0 ? "warning" : "primary";
+		classes += this.props.counter.value === 0 ? "warning" : "primary";
 		return classes;
 	}
 
 	formatCount() {
-		const { value: count } = this.state;
+		const { value: count } = this.props.counter;
 		return count === 0 ? "Zero" : count;
 	}
 }
