de5d61f25501dc349ea2a67b6f6b0842abbc4ffb
Ben Silveston <bsilveston37@gmail.com>
2020-08-19 16:39:02 +0100

    Enabled object destructuring for both components.

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,19 +2,17 @@ import React from "react";
 import Counter from "./counter";
 
 const Counters = (props) => {
+	const { counters, onReset, onDelete, onIncrement } = props;
 	return (
 		<div>
-			<button
-				onClick={props.onReset}
-				className="btn btn-primary btn-sm m-2"
-			>
+			<button onClick={onReset} className="btn btn-primary btn-sm m-2">
 				Reset
 			</button>
-			{props.counters.map((counter) => (
+			{counters.map((counter) => (
 				<Counter
 					key={counter.id}
-					onDelete={props.onDelete}
-					onIncrement={props.onIncrement}
+					onDelete={onDelete}
+					onIncrement={onIncrement}
 					counter={counter}
 				></Counter>
 			))}
